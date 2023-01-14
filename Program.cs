7850305f5c20386468d193e45a7b2eedf28cd48b
tjdiegoss <tjdiegoss@gmail.com>
2023-01-14 12:10:40 -0300

    Primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n = new Nokia("1234", "LumiaX19", "lkmcmsd8937", 64);
+n.InstalarAplicativo("Instagram");
+n.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Iphone ap = new Iphone("14587", "Iphone13 ProMax", "dsfs5454sdf", 120);
+ap.InstalarAplicativo("Whatsapp");
+ap.Ligar();
